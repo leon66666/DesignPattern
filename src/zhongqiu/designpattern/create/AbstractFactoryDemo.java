5e9f0bf16aa5247588a0fe31ca44bd273fa24ec0
leon66666 <1902518544@qq.com>
2018-01-16 18:30:28 +0800

    first version

--- src/zhongqiu/designpattern/create/AbstractFactoryDemo.java
+++ src/zhongqiu/designpattern/create/AbstractFactoryDemo.java
@@ -7,29 +7,29 @@ import zhongqiu.designpattern.create.util.SmsSender;
 
 //抽象工厂模式
 public class AbstractFactoryDemo {
-	public static void main(String[] args) {
-		Provider provider = new SendMailFactory();
-		Sender sender = provider.produce();
-		sender.Send();
-	}
+    public static void main(String[] args) {
+        Provider provider = new SendMailFactory();
+        Sender sender = provider.produce();
+        sender.Send();
+    }
 
-	public interface Provider {
-		public Sender produce();
-	}
+    public interface Provider {
+        public Sender produce();
+    }
 
-	public static class SendMailFactory implements Provider {
+    public static class SendMailFactory implements Provider {
 
-		@Override
-		public Sender produce() {
-			return new MailSender();
-		}
-	}
+        @Override
+        public Sender produce() {
+            return new MailSender();
+        }
+    }
 
-	public static class SendSmsFactory implements Provider {
+    public static class SendSmsFactory implements Provider {
 
-		@Override
-		public Sender produce() {
-			return new SmsSender();
-		}
-	}
+        @Override
+        public Sender produce() {
+            return new SmsSender();
+        }
+    }
 }
