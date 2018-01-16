83bc2be203db0ef7298a20e638414223ed90ba51
leon66666 <1902518544@qq.com>
2018-01-16 18:44:25 +0800

    first version

--- src/zhongqiu/designpattern/test/test.java
+++ src/zhongqiu/designpattern/test/test.java
@@ -9,38 +9,19 @@ import java.util.concurrent.Executors;
  * Created by wangzhongqiu on 2017/6/10.
  */
 public class Test {
-    int a = 1;
-    int b = 2;
+    private Test() {
 
-    public void change() {
-        a = 3;
-        b = a;
     }
 
-    public void recover() {
-        a = 1;
-        b = 2;
+    private static class factory {
+        private static Test instance = new Test();
     }
 
-    public void print() {
-        System.out.println("a=" + a + ";b=" + b);
-        if (a == 3 && b == 2) {
-            System.out.println("a=" + a + ";b=" + b);
-        }
+    public static Test getInstance() {
+        return factory.instance;
     }
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(1000);
-        final Test test = new Test();
-        while (true) {
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    test.recover();
-                    test.change();
-                    test.print();
-                }
-            });
-        }
+        Test.getInstance();
     }
 }
