73f498747530c31578a8971ff3036f561fa239a0
leon66666 <1902518544@qq.com>
2018-01-16 17:27:53 +0800

    first version

--- src/zhongqiu/designpattern/create/StaticFactoryDemo.java
+++ src/zhongqiu/designpattern/create/StaticFactoryDemo.java
@@ -3,19 +3,16 @@ package zhongqiu.designpattern.create;
 //静态工厂模式
 public class StaticFactoryDemo {
 
-	public static class SendFactory {
+	public static Sender produceMail() {
+		return new MailSender();
+	}
 
-		public static Sender produceMail() {
-			return new MailSender();
-		}
-
-		public static Sender produceSms() {
-			return new SmsSender();
-		}
+	public static Sender produceSms() {
+		return new SmsSender();
 	}
 
 	public static void main(String[] args) {
-		Sender sender = SendFactory.produceMail();
+		Sender sender = StaticFactoryDemo.produceMail();
 		sender.Send();
 	}
 }
